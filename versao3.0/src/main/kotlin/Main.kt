4a57fb96e1fb0b14fc88bce6cbe837c01c631299
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2022-03-09 19:49:38 -0300

    commit1 - 3.0

---
+++ versao3.0/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+import model.JogodaForca
+fun main(args: Array<String>) {
+    var j = JogodaForca("Casado", "Moradia - Sinonimo")
+    j.IniciarJogo()
+}
+/*
+* Jogo da forca
+*
+* Tentativas: 6
+* Palavra: Pao
+* Letras distintas: 3
+*
+*   - - -
+*   P - -
+*   P - O
+*   P A O
+* ->
+*
+* */
